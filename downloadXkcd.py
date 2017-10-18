97ec24c08568a94f9f926a95d713f90990884490
Ethan Axline <eaxline@icloud.com>
2017-10-17 22:43:14 -0400

    first commit

---
+++ downloadXkcd.py
@@ -0,0 +1,38 @@
+#! python3
+# downloadXkcd.py - Downloads all the XKCD comics..
+
+import os
+import requests, bs4
+
+url = 'http://xkcd.com'
+os.makedirs('xkcd', exist_ok=True)
+
+while not url.endswith('#'):
+    # download page
+    print('Downloading page %s...' % url)
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text)
+
+    # find url of comics
+    comicElem = soup.select('#comic img')
+    if comicElem == []:
+        print('Could not find comic image.')
+    else:
+        comicUrl = comicElem[0].get('src')
+    # download image
+        print('Downloading image %s...' % (comicUrl))
+        res = requests.get(comicUrl)
+        res.raise_for_status()
+    # save the image to ./xkcd
+    imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+    for chunk in res.iter_content(100000):
+        imageFile.write(chunk)
+    imageFile.close()
+
+    # get the prev buttons url
+    prevLink = soup.select('a[rel="prev"]')[0]
+    url = 'http://xkcd.com' + prevLink.get('href')
+
+print("Done..")
